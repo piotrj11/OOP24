02ec254251f078054954635c3b2a99faeb42a57a
Piotr Jedynak <w69941@student.wsiz.edu.pl>
2024-10-17 11:24:25 +0200

    first commit

---
+++ OOP/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OOP.Constructions;
+
+Console.WriteLine("Hello, World!");
+var construction = new Construction();
+var construction2 = new Construction(23.0f, 45.3f, 2, 3, "Material");
+Console.WriteLine("Hello, World!");
